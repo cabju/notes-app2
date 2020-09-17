eef27103bd839804c8a0ada00876d98aa26d3c6e
Carlo <Carlo_junior@hotmail.co.uk>
2020-09-17 17:39:32 +0100

    chapter 6 done

--- note-controller.js
+++ note-controller.js
@@ -1,1 +1,12 @@
-document.getElementById("app").innerHTML = "whats up?"
+//document.getElementById("app").innerHTML = "whats up?"
+class NoteController {
+  constructor() {
+  this.notelistview = new NoteListView()
+  this.notelistview.notelist.addNewNote("Favourite drink: seltzer")
+  }
+  shownote(){
+    document.getElementById("app").innerHTML = this.notelistview.showlist()
+  }
+}
+var notes=new NoteController();
+notes.shownote()
